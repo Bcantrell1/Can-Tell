72d89aff132a3425f5c01c00d96359708d4977b4
Bcantrell1_WSL <bribri546@gmail.com>
2025-05-22 23:22:06 -0700

    Start on SMTP testing of domain

--- smtp.go
+++ smtp.go
@@ -1,8 +1,40 @@
 package verifier
 
+import (
+	"time"
+)
+
 type SMTP struct {
 	HasHost     bool `json:"has_host"`
 	InboxFull   bool `json:"inbox_full"`
 	Deliverable bool `json:"deliverable"`
 	Banned      bool `json:"banned"`
+}
+
+func (v *Verifier) CheckWithSMTP(domainName, username string) (*SMTP, error) {
+	if !v.useSMTP {
+		return nil, nil
+	}
+	var smtp SMTP
+	var err error
+
+	// Check any SMTP server that is available
+	client, err := newSMTPClient(domainName, v.connectTimeout, v.operationTimeout)
+	if err != nil {
+		return nil, err
+	}
+
+	defer client.Close()
+
+	smtp.HasHost = true
+
+	if username == "" {
+		return &smtp, nil
+	}
+
+	return &smtp, nil
+}
+
+func newSMTPClient(domainName string, connectTimeout, operationTimeout time.Duration) (*smtpClient, error) {
+	return nil, nil
 }
